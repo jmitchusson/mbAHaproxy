a17aa4b2647f3713dcae91dc2ace31e8b8a25253
jmitchuson <jmitchusson23@gmail.com>
2020-02-04 16:16:29 -0600

    config basic html page

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,12 @@
+require 'socket'
+ip_list = Socket.ip_address_list
+ip = String.try_convert(ip_list.at(1).ip_address)
+
 default['mb_apache_haproxy']['java'] = 'java-1.6.0-openjdk'
 default['mb_apache_haproxy']['httpd'] = 'httpd'
+
+default['index.html']['title'] = 'Authored by Jane Doe'
+default['index.html']['body'] = 'Hello world!'
+default['index.html']['ipaddy'] = ip
+
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,8 @@ end
 service node['mb_apache_haproxy']['httpd'] do
   action [:enable, :start]
 end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  notifies :reload, 'service[httpd]', :immediately
+end
